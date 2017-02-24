1fc5f9bb44108ceda6c495b383b0ba2f52cf40b7
S <s>
2017-02-24 01:01:06 -0800

    {"types":"C","runs":51}

--- YahtzeeConstants.java
+++ YahtzeeConstants.java
@@ -35,6 +35,7 @@ public interface YahtzeeConstants {
 /** The constants about fixed points when player gets some of the lower scores */	
 	public static final int PTS_FULL_HOUSE = 25;
 	public static final int PTS_YAHTZEE = 50;
+	public static final int PTS_LG_STRT = 50;
 
 /** The constants that specify categories on the scoresheet */
 	public static final int ONES = 1;
